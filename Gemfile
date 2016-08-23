02f8b25c0099e58cb6b760a32f2e5055f639e9ff
D8vjörk <d8vjork@outlook.com>
2016-08-23 13:50:14 +0200

    To dev 0.2.0
    More info in changelog

--- Gemfile
+++ Gemfile
@@ -4,5 +4,3 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem 'compass', '~> 1.0', '>= 1.0.3'
-gem 'cssmin', '~> 1.0', '>= 1.0.3'
-gem 'jsmin', '~> 1.0', '>= 1.0.1'
--- config.rb
+++ config.rb
@@ -3,20 +3,19 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "dist/css"
 sass_dir = "src/sass"
+css_dir = "src/css"
 images_dir = "dist/images"
 javascripts_dir = "dist/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
+line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
